33fab20f875ff240a4ac61f44ba28a971ec5cece
gabequinto <78443183+gabequinto@users.noreply.github.com>
2021-10-02 22:45:40 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+# settings.py
+
+DATABASES = {
+     'default': {
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'postgres',
+          'USER': 'gabe',
+          'PASSWORD': 'sys265',
+          'HOST': 'db',
+          'PORT': 5432,
+     }
+}
