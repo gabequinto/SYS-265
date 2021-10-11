e26bc1d4294f233b8244101b80bdccb9fa151d61
gabequinto <78443183+gabequinto@users.noreply.github.com>
2021-10-11 16:43:08 -0400

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,11 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-
+#!/bin/bash
+sudo useraddd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+cd /home/gabe/SYS-265/linux/public-keys
+sudo cp id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh/
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
