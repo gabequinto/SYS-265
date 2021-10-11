879b6a2a34a295f10bc4d3fef06d69dc781de86b
gabequinto <78443183+gabequinto@users.noreply.github.com>
2021-10-11 16:47:31 -0400

    Update secure-ssh.sh

--- secure-ssh.sh
+++ secure-ssh.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-sudo useraddd -m -d /home/$1 -s /bin/bash $1
-sudo mkdir /home/$1/.ssh
-cd /home/gabe/SYS-265/linux/public-keys
-sudo cp id_rsa.pub /home/$1/.ssh/authorized_keys
-sudo chmod 700 /home/$1/.ssh/
-sudo chmod 600 /home/$1/.ssh/authorized_keys
-sudo chown -R $1:$1 /home/$1/.ssh
+
